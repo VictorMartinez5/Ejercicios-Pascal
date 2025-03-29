4a7b47cf6cb0504f202a1f16c9905bda0fe167ea
VictorMartinez5 <victorm050402@gmail.com>
2025-03-29 03:14:56 -0300

    ejercicio 17

--- ejercicio16/Ejercicio16.pas
+++ ejercicio16/Ejercicio16.pas
@@ -7,28 +7,28 @@ Program porcentajeCurso;
 
 Uses crt;
 
-Var ninas, ninos, totalAlumnos: integer;
+Var ninas, ninos, totalAlumnos:   integer;
 
-Var porcentajeNinas, porcentajeNinos: real;
+Var porcentajeNinas, porcentajeNinos:   real;
 
 Begin
-  clrscr;
-  writeln('Ingrese la cantidad de alumnos');
-  readln(ninos);
-  writeln('Inagrese la cantidas de alumnas');
-  readln(ninas);
+    clrscr;
+    writeln('Ingrese la cantidad de alumnos');
+    readln(ninos);
+    writeln('Inagrese la cantidas de alumnas');
+    readln(ninas);
 
-  totalAlumnos := ninos+ninas;
-  If (totalAlumnos > 0) Then
-    Begin
-      porcentajeNinos := (ninos/totalAlumnos)*100;
-      porcentajeNinas := (ninas/totalAlumnos)*100;
+    totalAlumnos := ninos+ninas;
+    If (totalAlumnos > 0) Then
+        Begin
+            porcentajeNinos := (ninos/totalAlumnos)*100;
+            porcentajeNinas := (ninas/totalAlumnos)*100;
 
-      writeln('El porcentaje de ninos es de ', porcentajeNinos:0:2, '%');
-      writeln('El porcentaje de ninas es  de ', porcentajeNinas:0:2, '%');
-    End
-  Else
-    writeln('No hay estudiantes en el curso');
+            writeln('El porcentaje de ninos es de ', porcentajeNinos:0:2, '%');
+            writeln('El porcentaje de ninas es  de ', porcentajeNinas:0:2, '%');
+        End
+    Else
+        writeln('No hay estudiantes en el curso');
 
-  readln;
+    readln;
 End.
---
+++ ejercicio17/ejercicio17.pas
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+{Se ingresan tres números enteros. Determina si al menos dos de los tres números
+son iguales. 
+Si esto es cierto, muestra un mensaje indicando "Hay números repetidos". 
+Si no, muestra un mensaje indicando "No hay números repetidos".}
+
+Program numerosEnteros;
+
+Uses crt;
+
+Var n1, n2, n3:   integer;
+
+Begin
+    writeln('Ingrese los tres numeros');
+    readln(n1, n2, n3);
+    If (n1 = n2) Or (n1 = n3) Or (n2 = n3) Then
+        Begin
+
+            writeln('Hay numeros repetidos');
+        End
+    Else
+        writeln('No hay nueros repetidos');
+    readln;
+End.
